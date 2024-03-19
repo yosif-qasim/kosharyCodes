2f438743ea7151e28a11240b60a5df6a9842973f
yosif-qasim <93684533+yosif-qasim@users.noreply.github.com>
2024-03-19 03:25:55 +0200

    create project

---
+++ syntaxy.js
@@ -0,0 +1,6 @@
+var target  = document.getElementById( 'syntaxy' );
+var syntaxy = new Syntaxy( target, {} );
+syntaxy.render();
+var target2  = document.getElementById( 'syntaxy2' );
+var syntaxy2 = new Syntaxy( target2, {} );
+syntaxy2.render();
